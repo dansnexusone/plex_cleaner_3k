fe411a8437b254c495ddc03b8ce94199347edd5b
Dan Strong <daniel.strong@workday.com>
2025-01-10 10:37:21 -0500

    Use OverseerrConfig object

--- services/overseerr.py
+++ services/overseerr.py
@@ -2,31 +2,33 @@ from typing import List
 
 import requests
 
-from models.config import Config
+from models.config import OverseerrConfig
 
 
 class OverseerrService:
-    def __init__(self, config: Config):
-        self.config = config
+    def __init__(self, overseerr_config: OverseerrConfig):
+        self.config = overseerr_config
+
+        # Set up session
         self.session = requests.Session()
         self._authenticate()
 
     def _authenticate(self) -> None:
-        url = f"{self.config.overseerr_url}/api/v1/auth/local"
+        url = f"{self.config.url}/api/v1/auth/local"
         response = self.session.post(
             url,
             headers=self._get_headers(),
-            json={"email": self.config.overseerr_email, "password": self.config.overseerr_password},
+            json={"email": self.config.email, "password": self.config.password},
         )
         response.raise_for_status()
 
     def _get_headers(self) -> dict:
-        return {"X-Api-Key": self.config.overseerr_api_key}
+        return {"X-Api-Key": self.config.api_key}
 
     def get_all_requests(self) -> List[any]:
         all_requests = []
         for page in range(1, 10):
-            url = f"{self.config.overseerr_url}/api/v1/request"
+            url = f"{self.config.url}/api/v1/request"
             params = {"take": 250, "skip": (page - 1) * 250, "sort": "added", "filter": "available"}
             response = self.session.get(url, headers=self._get_headers(), params=params)
             response.raise_for_status()
